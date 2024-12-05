7defba31f92cfb2bea1912f5daa6421e657fecc7
Raayavegas <veeralarithika@gmail.com>
2024-12-05 13:30:31 +0530

    Add files via upload

---
+++ forodd.java
@@ -0,0 +1,11 @@
+class A 
+{
+    public static void main(String args[])
+    {
+        int n=10;
+        for(int i=1;i<=10;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
